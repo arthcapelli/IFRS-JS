4d4578485f3241e03f24f5eaf77b860de9f2e40b
Arthur Capelli <71565674+arthcapelli@users.noreply.github.com>
2021-01-11 16:47:02 -0300

    Add files via upload

---
+++ Outros/meucodigo.js
@@ -0,0 +1,8 @@
+var a = 12.5;
+var b =3;
+var c = a+b;
+alert("Total da soma: " + c);
+
+var nome;
+nome = prompt("Informe seu nome: ");
+alert("Olá, " + nome);
---
+++ Outros/parate10.js
@@ -0,0 +1,9 @@
+var inicio = 2
+var fim = 10
+var todos = "Números pares: "
+
+while(inicio <= fim){
+	todos = todos + inicio + ", "
+	inicio = inicio + 2
+}
+alert(todos)
